2b13ecb0f3bf4bea8051478bbb516b31bd610c31
niknaknel <niknaknel@gmail.com>
2025-05-03 02:30:45 +0200

    basic scoring working. still need to adjust details

---
+++ src/attraction.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class Attraction(Enum):
+    HISTORICAL = "historical"
+    ENTERTAINMENT = "entertainment"
+    FOOD = "food"
+    SHOPPING = "shopping"
+    PARKS = "parks"
+    ARTS = "arts"
+    ADVENTURE = "adventure"
+    WELLNESS = "wellness"
